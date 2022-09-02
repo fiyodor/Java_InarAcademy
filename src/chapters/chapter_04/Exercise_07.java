749d468244852d12059890384bebacf9c6a95e9c
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-09-02 23:00:08 +0300

    Exercises 3 and 7 modified

--- src/chapters/chapter_04/Exercise_07.java
+++ src/chapters/chapter_04/Exercise_07.java
@@ -6,36 +6,43 @@ public class Exercise_07 {
 	
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
+		Scanner input= new Scanner (System.in);
 		
-		System.out.print("Enter the radius of the bounding circle: ");
+		System.out.println("Enter the radius of the bounding circle:");
 		
-		double radius = input.nextDouble();
+		double r= input.nextDouble();
 		
-		double x1 = 0;
-		double y1 = radius * Math.sin(2.0 * Math.PI / 5.0 * 1);
+		double angle1=(Math.PI / 2);
+		double angle2=(Math.PI / 2) + 2 * Math.PI / 5;
+		double angle3=(Math.PI / 2) + 2 * Math.PI / 5 + 2 * Math.PI / 5 ;
+		double angle4=(Math.PI / 2) + 2 * Math.PI / 5 + 2 * Math.PI / 5 + 2 * Math.PI / 5 ;
+		double angle5=(Math.PI / 2) + 2 * Math.PI / 5 + 2 * Math.PI / 5 + 2 * Math.PI / 5 +  2 * Math.PI / 5 ;
 		
-		double x2 = radius * Math.cos(2.0 * Math.PI / 5.0 * 2);
-		double y2 = radius * Math.sin(2.0 * Math.PI / 5.0 * 2);
+		double x1= ((int)(r * Math.cos(angle1)) * 100) / 100.0;
+		double y1= r * Math.sin(angle1);
 		
-		double x3 = radius * Math.cos(2.0 * Math.PI / 5.0 * 3);
-		double y3 = radius * Math.sin(2.0 * Math.PI / 5.0 * 3);
+		double x2= r * Math.cos(angle2);
+		double y2= r * Math.sin(angle2);
 		
-		double x4 = radius * Math.cos(2.0 * Math.PI / 5.0 * 4);
-		double y4 = radius * Math.sin(2.0 * Math.PI / 5.0 * 4);
+		double x3= r * Math.cos(angle3);
+		double y3= r * Math.sin(angle3);
 		
-		double x5 = radius * Math.cos(2.0 * Math.PI / 5.0 * 5);
-		double y5 = radius * Math.sin(2.0 * Math.PI / 5.0 * 5);
+		double x4= r * Math.cos(angle4);
+		double y4= r * Math.sin(angle4);
 		
-		System.out.println("x1 y1 is (" + x1 + " , " + y1 + ")" );
+		double x5= r * Math.cos(angle5);
+		double y5= r * Math.sin(angle5);
 		
-		System.out.println("x2 y2 is (" + x2 + " , " + y2 + ")" );
+		System.out.println("The coordinates of five points on the pentagon are");
 		
-		System.out.println("x3 y3 is (" + x3 + " , " + y3 + ")" );
+		System.out.println("Point 1  : ( " + x1 + "," + y1 + ")");
+		System.out.println("Point 2  : ( " + x2 + "," + y2 + ")");
+		System.out.println("Point 3  : ( " + x3 + "," + y3 + ")");
+		System.out.println("Point 4  : ( " + x4 + "," + y4 + ")");
+		System.out.println("Point 5  : ( " + x5 + "," + y5 + ")");
 		
-		System.out.println("x4 y4 is (" + x4 + " , " + y4 + ")" );
 		
-		System.out.println("x5 y5 is (" + x5 + " , " + y5 + ")" );
+
 	}
 
 }
